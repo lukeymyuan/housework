84a458d38931520de5f3b08e12fdf790e2a7f16f
lyuan <Luke.Yuan@macquarie.com>
2020-04-05 21:59:51 +1000

    added wheel

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,10 +1,22 @@
 import Taro, { Component, chooseInvoiceTitle } from '@tarojs/taro'
 import { View, Image, Text, Button } from '@tarojs/components'
-import { AtModal, AtModalHeader, AtModalContent, AtModalAction, AtButton, AtTag, AtNoticebar } from "taro-ui"
+import { AtModal, AtModalHeader, AtModalContent, AtModalAction, AtButton, AtTag, AtNoticebar, AtCurtain } from "taro-ui"
 import Tab from './../../components/tab/tab'
+import Luckyturntable from 'wechat-taroturntable'
+
 import familyHouseworkJpg from './family-housework.jpg'
 
+
 const actions = { tag1: "做饭", tag2: "洗碗", tag3: "拖地", tag4: "倒垃圾", tag5: "洗衣服", tag6: "买菜", tag7: "刷马桶", tag8: "铲屎" };
+
+const awardsList = [
+  { id: 1, award: '娱乐放松嗨', comment: '电影要去影院看，飙歌要去包间喊！', commentIcon: 'sound' },
+  { id: 2, award: '美味要大块', comment: '垂涎直下三千尺，享用美食在眼前。', commentIcon: 'heart' },
+  { id: 3, award: '独坐静发呆', comment: '放空一段自己自由自我的午后，静静品味岁月静好。', commentIcon: 'user' },
+  { id: 4, award: '心念下单买', comment: '终于为心心念念的消费找到一个充分必要的理由！', commentIcon: 'shopping-cart' },
+  { id: 5, award: '开溜百里外', comment: '背上包，说走就走。多久没有出游玩一趟了？', commentIcon: 'image' },
+  { id: 6, award: '红包约人派', comment: '朝某人振臂一呼，小小激励速速！且行且珍惜.' }
+];
 
 export default class Index extends Component {
   config = {
@@ -37,7 +49,8 @@ export default class Index extends Component {
       tag6: false,
       tag7: false,
       tag8: false,
-      activeTags: []
+      activeTags: [],
+      isCurtainOpened: false
     }
   }
 
@@ -50,7 +63,7 @@ export default class Index extends Component {
     const result = tags.filter(tag => this.state[tag] === true);
     this.setState({ activeTags: result }, () => {
       const action = actions[this.state.activeTags[Math.floor(Math.random() * this.state.activeTags.length)]];
-      this.setState({ action: action, isOpened: true });
+      this.setState({ action: action, isCurtainOpened: true });
     })
 
   }
@@ -58,6 +71,12 @@ export default class Index extends Component {
   handleModalClose() {
     this.setState({
       isOpened: false
+    })
+  }
+
+  onCurtainClose() {
+    this.setState({
+      isCurtainOpened: false
     })
   }
 
@@ -82,6 +101,13 @@ export default class Index extends Component {
           <AtModalHeader>我来{this.state.action}</AtModalHeader>
           <AtModalAction> <Button onClick={this.handleModalClose}>确定</Button> </AtModalAction>
         </AtModal>
+
+        <AtCurtain
+          isOpened={this.state.isCurtainOpened}
+          onClose={this.onCurtainClose.bind(this)}
+        >
+          <Luckyturntable awards={awardsList} buttonTitle='小鼓励' />
+        </AtCurtain>
 
         <View className='at-row at-row__justify--center'>
           <View className='at-col at-col-3'><AtTag name='tag1' type='primary' active={this.state.tag1} onClick={this.handleTagClick}>
